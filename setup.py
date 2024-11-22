e5815057ae618940dafc167d9d05f8158b017f3d
Chancelor Roberts <88689227+chancelorr@users.noreply.github.com>
2024-11-21 18:48:58 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,5 @@ from setuptools import setup, find_packages
 setup(
     name="ATL11-tools",
     version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        'os', 'sys', 'h5py', 'configparser', 'json', 'pyogrio', 'numpy', 'pandas', 
-        'geopandas', 'xarray', 'rasterio', 'shapely', 'datetime', 'time', 'matplotlib', 
-        'cmcrameri', 'contextily', 'earthpy', 'utils', 'traceback'
-    ],
+    packages=find_packages()
 )
